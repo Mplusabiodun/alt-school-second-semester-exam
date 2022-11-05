9b0c2dc4e77d75e992abd2f004e3d16463bd63de
Mplusabiodun <fataimonsuru2020@gmail.com>
2022-11-05 14:43:38 +0100

    rv unused component from app.js and useCounter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Router from './router/Router';
-import Header from './components/Header';
+// import Header from './components/Header';
 
 
 function App() {
--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useCounter = (init) => {
   const [counter, setCounter] = useState(init);
